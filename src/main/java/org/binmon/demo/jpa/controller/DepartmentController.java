e9c3129e4da59d7438de01145ea0459e3383b30a
Ananda Bhaumik <bhaumikananda@gmail.com>
2018-11-03 23:08:06 +0530

    Test messages

--- src/main/java/org/binmon/demo/jpa/controller/DepartmentController.java
+++ src/main/java/org/binmon/demo/jpa/controller/DepartmentController.java
@@ -2,13 +2,22 @@ package org.binmon.demo.jpa.controller;
 
 import java.util.List;
 
+import org.binmon.demo.DemoApplication;
 import org.binmon.demo.jpa.domain.Dept;
 import org.binmon.demo.jpa.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,29 +25,47 @@ public class DepartmentController {
 
 	@Autowired
 	DepartmentService deptService;
+	
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
-	@RequestMapping(method=RequestMethod.GET, value="/dept/list")
-	public List<Dept> getDepartments() {
-		return deptService.getDepartments();
+	@GetMapping("/dept/list")
+	public ResponseEntity<List<Dept>> getAllDept(@RequestHeader(value="myHeader") String headerStr) {
+	    logger.info("this is a info message");
+	    logger.warn("this is a warn message");
+	    logger.error("this is a error message");
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("getAllDept", "ReturningListOfDepartments");
+		return new ResponseEntity<List<Dept>>(deptService.getDepartments(), headers, HttpStatus.OK);
 	}
 
-	@RequestMapping(method=RequestMethod.GET, value="/dept/{id}")
-	public Dept getDepartmens(@PathVariable("id") String id) {
-		return deptService.getDepartment(id);
+	@GetMapping("/dept/{id}")
+	public ResponseEntity<Dept> getDept(@PathVariable("id") String id) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("getSelectDept", "ReturningParticularDepartment");
+		return new ResponseEntity<Dept>(deptService.getDepartment(id), headers, HttpStatus.OK);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, value="/dept/add")
+	@PostMapping("/dept/add")
 	public void addDept(@RequestBody Dept dept) {
 		deptService.addDepartment(dept);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, value="/dept/updt")
-	public void updtDept(@RequestBody Dept dept) {
+	@PutMapping("/dept/updt")
+	public void updateDept(@RequestBody Dept dept) {
 		deptService.updateDepartment(dept);
 	}
 	
-	@RequestMapping(method=RequestMethod.DELETE, value="/dept/dlt/{deptId}")
-	public void updtDept(@PathVariable("deptId") String deptId) {
-		deptService.deleteDepartment(deptId);
+	@DeleteMapping("/dept/dlt")
+	public ResponseEntity<Dept> deleteDept(@RequestBody Dept dept) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("wer", "See this is so nice");
+		if(deptService.deleteDepartment(dept.getDeptId())) {
+			System.out.println("For 1");
+			System.out.println(headers);
+			return new ResponseEntity<Dept>(dept, headers, HttpStatus.OK);
+		}
+		System.out.println("For 2");
+		return new ResponseEntity<Dept>(dept, headers, HttpStatus.OK);
 	}
+
 }
--- src/test/java/org/binmon/demo/DemoApplicationTests.java
+++ src/test/java/org/binmon/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package org.binmon.demo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +11,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Before
+	public void init() {
+		
+	}
+	
+	@After
+	public void clear() {
+		
+	}
+
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	
 }
