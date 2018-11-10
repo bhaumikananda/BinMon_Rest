7cace9e057c7b7f5ce5e055b43517ef51b370087
Ananda Bhaumik <bhaumikananda@gmail.com>
2018-11-10 23:37:02 +0530

    First mocking implementation.

--- src/test/java/org/binmon/demo/jpa/service/DepartmentServiceTest.java
+++ src/test/java/org/binmon/demo/jpa/service/DepartmentServiceTest.java
@@ -1,8 +1,14 @@
 package org.binmon.demo.jpa.service;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import org.binmon.demo.jpa.stub.DepartmentRepositoryStub;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.binmon.demo.jpa.domain.Dept;
+import org.binmon.demo.jpa.repository.DepartmentRepository;
 import org.junit.Test;
 
 public class DepartmentServiceTest {
@@ -16,8 +22,15 @@ public class DepartmentServiceTest {
 	@Test
 	public void testWithDB() {
 	DepartmentService dptServ = new DepartmentService();
-	dptServ.setDepartmentRepository(new DepartmentRepositoryStub());
-	assertTrue(dptServ.getDepartments().size()==5);
+	DepartmentRepository deptRepoStub = mock(DepartmentRepository.class);
+	when(deptRepoStub.findAll()).thenReturn(new ArrayList<Dept>(Arrays.asList(
+			new Dept("1", "Maths", "Bhaumik"),
+			new Dept("2", "Physics", "Sen"),
+			new Dept("Hi", "Physics", "Sen"),
+			new Dept("3", "French", "Mance")
+		)));
+	dptServ.setDepartmentRepository(deptRepoStub);
+	assertTrue(dptServ.getDepartments().size()==4);
 	}
 	
 
