350a1441108d0802cdd0d1bde540da3156048c41
Ananda Bhaumik <bhaumikananda@gmail.com>
2018-11-28 00:47:11 +0530

    FirstUnitTesting

--- src/test/java/org/binmon/demo/DepartmentControllerTests.java
+++ src/test/java/org/binmon/demo/DepartmentControllerTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class DepartmentControllerTests {
 
 	@Before
 	public void init() {
